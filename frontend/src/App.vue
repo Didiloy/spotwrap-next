87eb41fc0796d793763f0c21ba5def5479814ef8
Didiloy <didiloy2001@gmail.com>
2025-05-20 15:23:49 +0200

    fixed issue where the main trigger was shown on top of the sidebar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,25 +9,28 @@ import { useDownloadStore } from "@/store/download";
 
 const settingsStore = useSettingsStore();
 const downloadStore = useDownloadStore();
-const showMainTrigger = ref(true); // Default to showing the trigger
+const showMainTrigger = ref(false); // Default to showing the trigger
 
 onMounted(async () => {
-  // Initialize download store event listener
-  downloadStore.setupEventListener();
-  
-  // Check if Spotify credentials are valid
-  await settingsStore.loadSpotifyCredentials();
-  const hasValidCredentials = await settingsStore.checkCredentialsValidity();
-  
-  if (!hasValidCredentials) {
-    settingsStore.showCredentialsModal = true;
-  }
+    // Initialize download store event listener
+    downloadStore.setupEventListener();
+
+    // Check if Spotify credentials are valid
+    await settingsStore.loadSpotifyCredentials();
+    const hasValidCredentials = await settingsStore.checkCredentialsValidity();
+
+    if (!hasValidCredentials) {
+        settingsStore.showCredentialsModal = true;
+    }
 });
 </script>
 
 <template>
     <Toaster />
-    <SidebarProvider class="bg-zinc-100 h-screen flex" @update:open="showMainTrigger = !$event">
+    <SidebarProvider
+        class="bg-zinc-100 h-screen flex"
+        @update:open="showMainTrigger = !$event"
+    >
         <AppSidebar class="flex-shrink-0" />
         <main class="relative flex-grow h-full overflow-hidden p-4">
             <div v-if="showMainTrigger" class="fixed top-4 left-4 z-50">
@@ -36,8 +39,10 @@ onMounted(async () => {
             <router-view class="w-full h-full" />
         </main>
     </SidebarProvider>
-    
-    <SpotifyCredentialsModal v-model:open="settingsStore.showCredentialsModal" />
+
+    <SpotifyCredentialsModal
+        v-model:open="settingsStore.showCredentialsModal"
+    />
 </template>
 
 <style></style>
